476b7ad20b0501a3b0253574043fa04930cf36bc
Tom MacWright <tom@macwright.org>
2015-03-23 18:53:19 -0400

    Proper cycles

--- README.md
+++ README.md
@@ -10,6 +10,16 @@ $ npm install -g circular-require
 
 ## Use
 
+`circular-require` provides a single command that takes an entry point
+as its one argument. If it is run in a directory with a package.json
+with browserify.transform field, it'll use those transforms while resolving
+dependencies.
+
 ```sh
-$ circular-require index.js
+$ ~/src/app-styles〉circular-require src/index.js
+found 1 cycles
+[ '/Users/tmcw/src/app-styles/src/components/forms/_index.js',
+  '/Users/tmcw/src/app-styles/src/util/_compute_form.js',
+  '/Users/tmcw/src/app-styles/src/components/forms/ramps/generic_ramp.js',
+  '/Users/tmcw/src/app-styles/src/components/forms/generic_fn.js' ]
 ```
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mdeps = require('module-deps'),
     path = require('path'),
-    traverse = require('traverse');
+    graphlib = require('graphlib');
 
 var core = ['events', 'path', 'util', 'vm', 'dns', 'dgram', 'http', 'https', 'net', 'fs'];
 
@@ -15,7 +15,7 @@ try {
     transform = require(path.resolve('package.json')).browserify.transform;
 } catch(e) { }
 
-var entries = {};
+var g = new graphlib.Graph({ directed: true });
 var md = mdeps({
     transform: transform,
     filter: function(id) {
@@ -23,24 +23,20 @@ var md = mdeps({
     }
 });
 
-function make(id) {
-    if (entries[id]) return entries[id];
-    entries[id] = [];
-    return entries[id];
-}
-
 md.on('data', function(entry) {
-    var e = make(entry.id);
     Object.keys(entry.deps).forEach(function(dep) {
-        return e.push(make(entry.deps[dep]));
+        g.setEdge(entry.id, entry.deps[dep]);
     });
 });
 
 md.on('end', function() {
-    traverse(entries).map(function (x) {
-        if (this.circular) {
-            console.log('circular dependency: ' + this.path[0]);
-        }
+
+    var cycles = graphlib.alg.findCycles(g);
+
+    console.log('found %s cycles', cycles.length);
+
+    cycles.forEach(function(cycle) {
+        console.log(cycle);
     });
 });
 
